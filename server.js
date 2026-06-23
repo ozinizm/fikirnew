1608634bea7f66428f04b21fd4bcc84a9d3114a3
Oğuzhan Çankaya <oguzhancankaya43@gmail.com>
2026-06-23 11:35:33 +0300

    ci: add server.js startup file for cPanel Node.js App

---
+++ server.js
@@ -0,0 +1,20 @@
+const { createServer } = require('http')
+const { parse } = require('url')
+const next = require('next')
+
+const dev = false
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+// cPanel Passenger passes the port to bind dynamically in process.env.PORT
+const port = process.env.PORT || 3000
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url, true)
+    handle(req, res, parsedUrl)
+  }).listen(port, err => {
+    if (err) throw err
+    console.log(`> Ready on port ${port}`)
+  })
+})
